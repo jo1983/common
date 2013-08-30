14331dd0c14116ca074f3ba1cbe13bc9cf8b09c1
George Nash <georgen@quicinc.com>
2013-08-29 18:30:28 -0700

    ALLJOYN-2323 Fixed doxygen warning produced by Debug.h
    the functions marked with @internal should have been surrounded by @cond/@endcond flags to prevent
    doxygen from trying to produce documentation.
    
    Change-Id: Id85a509b2dd76ed6de9dd2ade6d0db42dc8a095d

--- inc/qcc/Debug.h
+++ inc/qcc/Debug.h
@@ -7,7 +7,7 @@
 /******************************************************************************
  *
  *
- * Copyright 2009-2011, Qualcomm Innovation Center, Inc.
+ * Copyright 2009-2011, 2013 Qualcomm Innovation Center, Inc.
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -28,9 +28,10 @@
 
 #include <stdio.h>
 
-
+/** @cond ALLJOYN_DEV */
 /** @internal DEBUG */
 #define QCC_MODULE "DEBUG"
+/** @endcond */
 
 /**
  * Macro for printing out error messagages.
@@ -122,6 +123,7 @@
 
 
 /**
+ * @cond ALLJOYN_DEV
  * @internal
  * Generalized macro for printing debug messages for code built in debug mode.
  *
@@ -140,9 +142,11 @@
         }                                                               \
     } while (0)
 #endif
+/** @endcond */
 
 
 /**
+ * @cond ALLJOYN_DEV
  * @internal
  * Generalized macro for dumping arrays of data.
  *
@@ -156,7 +160,7 @@
 #define _QCC_DbgDumpData(_msgType, _data, _len)                         \
     _QCC_DbgDumpHex((_msgType), QCC_MODULE, __FILE__, __LINE__, # _data, (_data), (_len))
 #endif
-
+/** @endcond */
 
 
 #ifdef __cplusplus
@@ -236,6 +240,7 @@ void QCC_RegisterOutputCallback(QCC_DbgMsgCallback cb, void* context);
 void QCC_RegisterOutputFile(FILE* file);
 
 /**
+ * @cond ALLJOYN_DEV
  * @internal
  * Creates and prepares a debug context for printing debug output.
  *
@@ -296,6 +301,7 @@ static class DebugInitializer {
     ~DebugInitializer();
 } debugInitializer;
 
+/** @endcond */
 #ifdef __cplusplus
 }
 #endif
