781cc5e43741c2767ef6a9040056f89a9164b8cc
Sheshambika Venkateshwaran <sheshamb@quicinc.com>
2013-10-01 08:04:05 -0700

    ALLJOYN-2430 Convert serial port setting to 8-N-1
    Change-Id: I81d1f83275fb9b01e60fff65bf0b2ce6170a7acc

--- os/posix/UARTStreamLinux.cc
+++ os/posix/UARTStreamLinux.cc
@@ -138,9 +138,9 @@ QStatus UART(qcc::String devName, uint32_t speed, UARTFd& fd)
     cfsetospeed(&ttySettings, baudrate);
     cfsetispeed(&ttySettings, baudrate);
 
-    /* baudrate - 8 - E - 1 */
+    /* baudrate - 8 - N - 1 */
     ttySettings.c_iflag |= INPCK;
-    ttySettings.c_cflag |= CS8 | CLOCAL | CREAD | PARENB;
+    ttySettings.c_cflag |= CS8 | CLOCAL | CREAD;
 
     tcflush(fd, TCIOFLUSH);
 
